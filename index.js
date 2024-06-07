bf5c140e4103ec83d837cf85597682814277c1c1
samojeyinka <samuelojeyinka@gmail.com>
2024-06-06 17:19:21 -1200

    Text prompt

---
+++ index.js
@@ -0,0 +1,17 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require("dotenv").config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const prompt =
+    "What do you know about the future";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
